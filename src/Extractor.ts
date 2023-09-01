9dc7bdbc2d49f86d93b27764d9cb789bbf1d0f1f
Ivo Limić <ilimic1@gmail.com>
2023-09-01 19:43:44 +0200

    separating extractor logic into a class and refactoring into smaller units so the code is testable.

---
+++ src/Extractor.ts
@@ -0,0 +1,97 @@
+import puppeteer, { Browser, Page } from 'puppeteer';
+
+export default class Extractor {
+  private browser: Browser | null = null;
+  private page: Page | null = null;
+
+  public async gotoLoginPage() {
+    this.browser = await puppeteer.launch({ headless: false });
+    this.page = await this.browser.newPage();
+    await this.page.goto('https://www.notion.so/login');
+  }
+
+  public async login(email: string, password: string) {
+    if (this.page === null) {
+      throw new Error('Page is null.');
+    }
+
+    await this.page.waitForSelector('#notion-email-input-2');
+    await this.page.type('#notion-email-input-2', email);
+    await this.page.keyboard.press('Enter');
+
+    await this.page.waitForSelector('#notion-password-input-1');
+    await this.page.type('#notion-password-input-1', password);
+    await this.page.keyboard.press('Enter');
+    // todo
+  }
+
+  public async waitForDashboard() {
+    if (this.page === null) {
+      throw new Error('Page is null.');
+    }
+
+    await this.page.waitForSelector('.notion-frame', {
+      timeout: 2 * 60 * 1000,
+    });
+
+    /**
+     * We need to get token_v2 and file_token from cookies.
+     * token_v2 will be set after login.
+     * file_token will be set after visiting https://www.notion.so/f/refresh
+     */
+
+    // set file_token cookie
+    const result = await this.page.evaluate(
+      "fetch('/f/refresh').then((res) => res.text()).then((response) => response);"
+    );
+
+    // await new Promise((r) => setTimeout(r, 5000));
+  }
+
+  public async getTokens() {
+    if (this.page === null) {
+      throw new Error('Page is null.');
+    }
+
+    // const cookies1 = await this.page.cookies('.notion.so');
+    // const cookies2 = await this.page.cookies('.notion.so/f');
+    // const cookies3 = await this.page.cookies('www.notion.so');
+    // const cookies4 = await this.page.cookies('www.notion.so/f');
+    // const cookies5 = await this.page.cookies('https://notion.so');
+    // const cookies6 = await this.page.cookies('https://notion.so/f'); // has file_token
+    // const cookies7 = await this.page.cookies('https://www.notion.so'); // has token_v2
+    // const cookies8 = await this.page.cookies('https://www.notion.so/f'); // has both token_v2 and file_token
+
+    const cookies = await this.page.cookies('https://www.notion.so/f'); // this domain has both token_v2 and file_token cookies
+
+    const tokenV2 = cookies.reduce<string | null>(
+      (tokenV2, cookie) =>
+        cookie.name === 'token_v2' ? cookie.value : tokenV2,
+      null
+    );
+
+    if (tokenV2 === null || tokenV2 === '') {
+      throw new Error('Could not get token_v2 from cookies.');
+    }
+
+    const fileToken = cookies.reduce<string | null>(
+      (fileToken, cookie) =>
+        cookie.name === 'file_token' ? cookie.value : fileToken,
+      null
+    );
+
+    if (fileToken === null || fileToken === '') {
+      throw new Error('Could not get file_token from cookies.');
+    }
+
+    return { tokenV2, fileToken };
+  }
+
+  public async closeBrowser() {
+    if (this.browser === null) {
+      throw new Error('Browser is null.');
+    }
+
+    await this.browser.close();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,11 @@
-import puppeteer from 'puppeteer';
+import Extractor from './Extractor';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://www.notion.so/login');
-
-  await page.waitForSelector('.notion-frame', { timeout: 2 * 60 * 1000 });
-
-  /**
-   * We need to get token_v2 and file_token from cookies.
-   * token_v2 will be set after login.
-   * file_token will be set after visiting https://www.notion.so/f/refresh
-   */
-
-  // set file_token cookie
-  const result = await page.evaluate(
-    "fetch('/f/refresh').then((res) => res.text()).then((response) => response);"
-  );
-
-  // await new Promise((r) => setTimeout(r, 5000));
-
-  // const cookies1 = await page.cookies('.notion.so');
-  // const cookies2 = await page.cookies('.notion.so/f');
-  // const cookies3 = await page.cookies('www.notion.so');
-  // const cookies4 = await page.cookies('www.notion.so/f');
-  // const cookies5 = await page.cookies('https://notion.so');
-  // const cookies6 = await page.cookies('https://notion.so/f'); // has file_token
-  // const cookies7 = await page.cookies('https://www.notion.so'); // has token_v2
-  // const cookies8 = await page.cookies('https://www.notion.so/f'); // has both token_v2 and file_token
-
-  const cookies = await page.cookies('https://www.notion.so/f'); // this domain has both token_v2 and file_token cookies
-
-  const tokenV2 = cookies.reduce<string | null>(
-    (tokenV2, cookie) => (cookie.name === 'token_v2' ? cookie.value : tokenV2),
-    null
-  );
-
-  if (tokenV2 === null || tokenV2 === '') {
-    throw new Error('Could not get token_v2 from cookies.');
-  }
-
-  const fileToken = cookies.reduce<string | null>(
-    (fileToken, cookie) =>
-      cookie.name === 'file_token' ? cookie.value : fileToken,
-    null
-  );
-
-  if (fileToken === null || fileToken === '') {
-    throw new Error('Could not get file_token from cookies.');
-  }
-
-  await browser.close();
+  const extractor = new Extractor();
+  await extractor.gotoLoginPage();
+  await extractor.waitForDashboard();
+  const { tokenV2, fileToken } = await extractor.getTokens();
+  await extractor.closeBrowser();
 
   console.log(`TOKEN_V2='${tokenV2.replaceAll("'", "\\'")}'`);
   console.log(`FILE_TOKEN='${fileToken.replaceAll("'", "\\'")}'`);
